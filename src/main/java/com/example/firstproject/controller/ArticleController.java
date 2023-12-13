9fba5f9521d60d33d190f523b7cb7b24d651bda1
jisupark <pswkih2@naver.com>
2023-12-13 15:14:42 +0900

    commit

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -7,7 +7,9 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j //로깅 기능을 위한 어노테이션 System.out.println은 시스템에 악영향을 끼침
 @Controller
@@ -37,4 +39,17 @@ public class ArticleController {
         return "";
 
     }
+
+@GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){ //@PathVariable은 url의 전달값을 컨트롤러에 매개변수로 가져온다.
+        log.info("id = " + id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);//값이있으면 articleEntity에 넣고 없으면 null
+        //2. 모델에 데이터 등록하기 (뷰 페이지에서 사용하기 위해 모델을 등록함)
+        model.addAttribute("article", articleEntity);
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+
+}
+
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor // 기본생성자 어노테이션
 @ToString
 @Entity //엔티티로 선언
 public class Article {
@@ -18,7 +20,6 @@ public class Article {
     private String title;
     @Column
     private String content;
-
 
 
 }
